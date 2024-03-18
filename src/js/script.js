319c49c9c4979f2cf9fdf66f3e33ff9fe6998ba2
Matias Ayesa <matiasayesa99@outlook.com>
2024-03-18 00:32:03 -0300

    canvas

--- src/js/script.js
+++ src/js/script.js
@@ -8,10 +8,7 @@ learn_more_button.addEventListener('click', () => {
 // * BANNER TWEENS
 const tl_banner = gsap.timeline()
 tl_banner
-    // .from('.name', {duration: 1.5, x: -200, opacity: 0, delay: 0.3})
-    // .from('.span', {duration: 1.5, y: -200, opacity: 0, delay: 0.1})
     .from('.mushroom_img', { duration: 2.5, x: 0, opacity: 0, delay: 1, ease: "bounce.inOut" })
-// .from('.learn_more', {duration: 1.5, y: 0, opacity: 0}, '<')
 
 // * CONTACT ME ICONS MOVEMENT
 const icons = document.querySelectorAll('.contact_icon_github, .contact_icon_linkedin, .contact_icon_email, .contact_icon_spotify, .contact_icon_discord')
@@ -25,6 +22,7 @@ icons.forEach(icon => {
     })
 })
 
+// * FOOTER MAIL AND CLIPBOARD
 const clipboard = document.querySelector('.clipboard')
 const clipboard_email = document.querySelectorAll('.clipboard, .footer_a')
 const copy = text => navigator.clipboard.writeText(text)
@@ -35,33 +33,36 @@ clipboard.addEventListener('mouseover', () => {
 clipboard.addEventListener('mouseout', () => {
     gsap.to('.clipboard', { y: 0, duration: 0.2 })
 })
-
+// * TOAST 
 clipboard_email.forEach(e => {
     e.addEventListener('click', () => {
         copy('matiasayesa99@outlook.com')
-    const Toast = Swal.mixin({
-        toast: true,
-        position: "top-start",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        width: "auto",
-        color: "rgb(153, 194, 77)",
-        background: 'black',
-        showclass: {
-            popup: 'swal2-show',
-            backdrop: 'swal2-backdrop-show',
-            icon: 'swal2-icon-show'
-        },
-        didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
-        }
-    });
-    Toast.fire({
-        iconHtml: '<i class="fa-regular fa-clipboard fa-2xs" style="color: #b3003e;"></i>',
-        title: "Email copied to clipboard!"
-    });
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-start",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            width: "auto",
+            color: "rgb(153, 194, 77)",
+            background: 'black',
+            showclass: {
+                popup: 'swal2-show',
+                backdrop: 'swal2-backdrop-show',
+                icon: 'swal2-icon-show'
+            },
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            },
+            customClass: {
+                content: 'swal-content',
+                title: 'swal-content2'
+            }
+        });
+        Toast.fire({
+            iconHtml: '<i class="fa-regular fa-clipboard fa-2xs" style="color: #b3003e;"></i>',
+            title: "Email copied to clipboard!"
+        });
     })
 });
-
